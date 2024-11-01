a8e42986b3d7875972f9465cd60a60e5e3cde80e
gregoruu <gregor.uustalu@gmail.com>
2024-11-01 17:15:53 +0200

    pealeht kaardi ja hoveriga

---
+++ pealeht.js
@@ -0,0 +1,32 @@
+
+
+document.querySelectorAll(".allPaths").forEach(e => {
+e.setAttribute('class', `allPaths ${e.id}`);
+e.addEventListener("mouseover", function () {
+    window.onmousemove=function (j) {
+        x = j.clientX
+        y = j.clientY
+        document.getElementById('Linnaosa').style.top = y-60  + 'px'
+        document.getElementById('Linnaosa').style.left = x +10 + 'px'
+    }
+    const classes=e.className.baseVal.replace(/ /g, '.')         
+    document.querySelectorAll(`.${classes}`).forEach(country =>{
+        country.style.fill = "black"
+    })
+    document.getElementById("Linnaosa").style.opacity = 1
+    
+    document.getElementById("name").innerText = e.id
+})
+e.addEventListener("mouseleave", function () {
+    const classes=e.className.baseVal.replace(/ /g, '.')
+    document.querySelectorAll(`.${classes}`).forEach(country =>{
+        country.style.fill = "#ececec"
+    })
+    document.getElementById("Linnaosa").style.opacity = 0
+})
+
+e.addEventListener("click",function(){
+    getUser(e.id)
+})
+
+})
